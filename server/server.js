dda1903d90393d3b0d96ff465ec91d2b42a70e7f
Alfandhi <chandrafajar356@gmail.com>
2024-06-26 21:56:54 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -40,7 +40,7 @@ passport.use(
     {
       clientID: process.env.CLIENTID,
       clientSecret: process.env.CLIENSECRET,
-      callbackURL: process.env.CALLBACK_URL || 'https://taskapp-api.vercel.app/auth/google/callback',
+      callbackURL: '/auth/google/callback',
       scope: ['profile', 'email'],
     },
     async (accessToken, refreshToken, profile, done) => {
